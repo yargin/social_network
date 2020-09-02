eb8929fe919b2e76fc109dc5b4c76b7364faca7a
Яргин Юрий <yargin.y@gmail.com>
2020-09-02 14:16:55 +0300

    added connections properties to JawsDB & Heroku-Postgre

--- dao/src/main/java/com/getjavajob/training/yarginy/socialnetwork/dao/entities/accounts/AccountsTable.java
+++ dao/src/main/java/com/getjavajob/training/yarginy/socialnetwork/dao/entities/accounts/AccountsTable.java
@@ -4,19 +4,19 @@ package com.getjavajob.training.yarginy.socialnetwork.dao.entities.accounts;
  * table Accounts columns
  */
 public final class AccountsTable {
-    public static final String TABLE = "Accounts";
-    public static final String ID = TABLE + '.' + "ID";
-    public static final String NAME = TABLE + '.' + "Name";
-    public static final String SURNAME = TABLE + '.' + "Surname";
-    public static final String PATRONYMIC = TABLE + '.' + "Patronymic";
+    public static final String TABLE = "accounts";
+    public static final String ID = TABLE + '.' + "id";
+    public static final String NAME = TABLE + '.' + "name";
+    public static final String SURNAME = TABLE + '.' + "surname";
+    public static final String PATRONYMIC = TABLE + '.' + "patronymic";
     public static final String SEX = TABLE + '.' + "sex";
-    public static final String BIRTH_DATE = TABLE + '.' + "Birth_date";
-    public static final String ICQ = TABLE + '.' + "Icq";
-    public static final String SKYPE = TABLE + '.' + "Skype";
-    public static final String EMAIL = TABLE + '.' + "Email";
-    public static final String ADDITIONAL_EMAIL = TABLE + '.' + "Additional_email";
-    public static final String COUNTRY = TABLE + '.' + "Country";
-    public static final String CITY = TABLE + '.' + "City";
+    public static final String BIRTH_DATE = TABLE + '.' + "birth_date";
+    public static final String ICQ = TABLE + '.' + "icq";
+    public static final String SKYPE = TABLE + '.' + "skype";
+    public static final String EMAIL = TABLE + '.' + "email";
+    public static final String ADDITIONAL_EMAIL = TABLE + '.' + "additional_email";
+    public static final String COUNTRY = TABLE + '.' + "country";
+    public static final String CITY = TABLE + '.' + "city";
 
     private AccountsTable() {
     }
