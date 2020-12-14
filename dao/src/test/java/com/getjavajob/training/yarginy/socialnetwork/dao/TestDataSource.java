975a02d56c2c9f3c8f680c208f85fc39584bd2e4
Яргин Юрий <yargin.y@gmail.com>
2020-12-14 21:40:16 +0300

    1. override TestDataSource read from properties 2. added pooling in context.xml

--- dao/src/test/java/com/getjavajob/training/yarginy/socialnetwork/dao/TestDataSource.java
+++ dao/src/test/java/com/getjavajob/training/yarginy/socialnetwork/dao/TestDataSource.java
@@ -3,6 +3,7 @@ package com.getjavajob.training.yarginy.socialnetwork.dao;
 import com.getjavajob.training.yarginy.socialnetwork.dao.factories.ddl.ScriptExecutorImpl;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 
+//todo maybe download hikari & place here instead non-pool DriverManager?
 public class TestDataSource extends DriverManagerDataSource {
     private static final String SCRIPT_DIR = "src/test/resources/H2/";
     private static final String SCRIPT = "run_creation.sql";
