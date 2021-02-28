aac0891e630dbe5423c740d3830e1afbf7e68bf4
Яргин Юрий <yargin.y@gmail.com>
2021-03-01 01:08:11 +0300

    deleted CommonApplicationContextProvider

--- web/src/main/webapp/js/confirmationDialog.js
+++ web/src/main/webapp/js/confirmationDialog.js
@@ -1,6 +1,6 @@
 function confirmation(confirmMessage, innerChecks) {
     // alert('in confirmation function' + innerChecks);
-    if (innerChecks !== 'undefined') {
+    if (innerChecks !== undefined) {
         if (innerChecks) {
             return confirm(confirmMessage);
         }
