4deda190337ee91794eba5394093166d0534a281
Яргин Юрий <yargin.y@gmail.com>
2021-06-07 14:24:20 +0300

    fixed notification

--- web/src/main/java/com/getjavajob/training/yarginy/socialnetwork/web/WebApplication.java
+++ web/src/main/java/com/getjavajob/training/yarginy/socialnetwork/web/WebApplication.java
@@ -4,11 +4,22 @@ import com.getjavajob.training.yarginy.socialnetwork.service.configuration.Servi
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Import;
+import org.springframework.core.env.Environment;
 
 @SpringBootApplication
 @Import(ServiceConfig.class)
 public class WebApplication {
+    private final Environment environment;
+
+    public WebApplication(Environment environment) {
+        this.environment = environment;
+        System.out.println(environment.getProperty("========================================================================="));
+        System.out.println(environment.getProperty("PORT"));
+        System.out.println(environment.getProperty("========================================================================="));
+    }
+
     public static void main(String[] args) {
+
         SpringApplication.run(WebApplication.class, args);
     }
 }
